39da75239913760ad1149fb190097f2450d04717
bieliaievays <bieliaievays@gmail.com>
2015-12-02 23:10:40 +0100

    First commit

---
+++ src/main/java/ocr/Application.java
@@ -0,0 +1,16 @@
+package ocr;
+
+/**
+ * Created by Iuliia on 02.12.2015.
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/ocr/Greeting.java
@@ -0,0 +1,23 @@
+package ocr;
+
+/**
+ * Created by Iuliia on 02.12.2015.
+ */
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
