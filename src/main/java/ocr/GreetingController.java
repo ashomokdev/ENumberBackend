8fd2a9797f57e71ec553617fd717e3d0cc1e64f0
bieliaievays <bieliaievays@gmail.com>
2015-12-05 22:24:44 +0100

    HTTP codea implemented

--- src/main/java/ocr/GreetingController.java
+++ src/main/java/ocr/GreetingController.java
@@ -1,9 +1,16 @@
 package ocr;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.concurrent.atomic.AtomicLong;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by Iuliia on 02.12.2015.
@@ -11,14 +18,82 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
+//    private static final String template = "Hello, %s!";
+//    private final AtomicLong counter = new AtomicLong();
+//
+//    @RequestMapping("/greeting")
+//    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+//        TesseractExecutor executor = new TesseractExecutorImpl();
+//        executor.execute();
+//        return new Greeting(counter.incrementAndGet(),
+//                String.format(template, name));
+//    }
 
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        TesseractExecutor executor = new TesseractExecutorImpl();
-        executor.execute();
-        return new Greeting(counter.incrementAndGet(),
-                String.format(template, name));
+    @RequestMapping(value="/upload", method=RequestMethod.GET)
+    public @ResponseBody String provideUploadInfo() {
+        return "You can upload a file by posting to this same URL.";
     }
+
+    @RequestMapping(value="/upload", method=RequestMethod.POST)
+    public @ResponseBody
+    ResponseEntity handleFileUpload(@RequestParam("name") String name,
+                                    @RequestParam("file") MultipartFile file){
+        if (!file.isEmpty()) {
+            try {
+                byte[] bytes = file.getBytes();
+                BufferedOutputStream stream =
+                        new BufferedOutputStream(new FileOutputStream(new File(name)));
+                stream.write(bytes);
+                stream.close();
+
+                String json = "You successfully uploaded file!";//convert entity to json
+                return new ResponseEntity(json, HttpStatus.ACCEPTED);
+
+            } catch (Exception e) {
+                return new ResponseEntity("You failed to upload file.", HttpStatus.BAD_REQUEST);
+
+            }
+        } else {
+            return new ResponseEntity("You failed to upload file because the file was empty.", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+//
+//    @RequestMapping(value = "/show", method = RequestMethod.GET)
+//    public String displayForm() {
+//        return "file_upload_form";
+//    }
+//
+//    @RequestMapping(value = "/save", method = RequestMethod.POST)
+//    public String save(
+//            @ModelAttribute("uploadForm") FileUploadForm uploadForm,
+//            Model map) {
+//
+//        List<MultipartFile> files = uploadForm.getFiles();
+//
+//        List<String> fileNames = new ArrayList<String>();
+//
+//        if(null != files && files.size() > 0) {
+//            for (MultipartFile multipartFile : files) {
+//
+//                String fileName = multipartFile.getOriginalFilename();
+//                fileNames.add(fileName);
+//                //Handle file content - multipartFile.getInputStream()
+//
+//            }
+//        }
+//
+//        map.addAttribute("files", fileNames);
+//        return "file_upload_success";
+//    }
 }
